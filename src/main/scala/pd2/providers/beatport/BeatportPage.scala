fd11c6b1069f63a62dd6095bda1411d3b69a568a
mkrasilnikov <mi.krasilnikov@gmail.com>
2021-05-29 23:48:39 +0300

    *

--- src/main/scala/pd2/providers/beatport/BeatportPage.scala
+++ src/main/scala/pd2/providers/beatport/BeatportPage.scala
@@ -1,18 +1,15 @@
 package pd2.providers.beatport
 
 import io.circe
+import io.circe.generic.auto._
 import io.circe.{CursorOp, Decoder, HCursor}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import sttp.model.Uri
-import io.circe.generic.auto._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser.JsoupDocument
-import pd2.providers.Pd2Exception.UnexpectedServiceResponse
 import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.model.Document
-import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{attr, attrs, element, elements}
-
-import java.time.temporal.TemporalAmount
-import java.time.{Duration, LocalDate}
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{element, elements}
+import pd2.providers.Pd2Exception.UnexpectedServiceResponse
+import sttp.model.Uri
+import java.time.Duration
 import scala.util.{Failure, Success, Try}
 
 case class BeatportPageArtist(id : Int, name : String, slug : String)
@@ -25,9 +22,9 @@ case class BeatportPageTrack(
   mix         : String,
   release     : String,
   label       : String,
-  duration    : Duration,
+  duration    : Option[Duration],
   previewUrl  : Uri,
-  key         : String
+  key         : Option[String]
 )
 
 case class BeatportPager(currentPage : Int, totalPages : Int)
@@ -39,44 +36,54 @@ object BeatportPage {
   def parse(html : String) : Either[UnexpectedServiceResponse, BeatportPage] = {
 
     val tryDoc = Try { browser.parseString(html) }
-
     tryDoc match {
       case Failure(exception) => Left(UnexpectedServiceResponse(exception.getMessage, html, Some(exception)))
       case Success(doc) =>
-        doc >?> element("script.data-objects") match {
-          case None => Left(UnexpectedServiceResponse("Could not find script.data-objects", html, None))
-          case Some(el) => {
-            val script = el.text
-            val playablesIndex = script.indexOf("window.playables=")
-              if (playablesIndex == -1)
-                Left(UnexpectedServiceResponse("Could not find window.playables=", html, None))
-              else {
-                val jsonStr = script.substring(playablesIndex + "window.playables=".length, script.indexOf("};") + 1)
-                for {
-                  tracks <- parseTracks(jsonStr)
-                  pager <- parsePager(doc)
-                } yield BeatportPage(pager, tracks)
-              }
-          }
-        }
+        for {
+          visibleTrackIds <- parseVisibleTrackIds(doc, html)
+          scriptTracks    <- parseTracksJson(doc, html)
+          pager           <- parsePager(doc)
+          // Битпорт иногда выдает в скрипте на странице больше треков, чем видно пользователю.
+          visibleTracks = visibleTrackIds.map(id => scriptTracks.filter(_.id == id).head)
+        } yield BeatportPage(pager, visibleTracks)
     }
   }
 
-  def parseTracks(jsonStr: String) : Either[UnexpectedServiceResponse, List[BeatportPageTrack]] = {
-    for {
-      json <- circe.parser.parse(jsonStr)
-        .left.map(pf => UnexpectedServiceResponse(pf.message, jsonStr, None))
-      tracks <- json.hcursor.downField("tracks").as[List[BeatportPageTrack]]
-        .left.map(dr => UnexpectedServiceResponse(dr.message ++ CursorOp.opsToPath(dr.history), jsonStr, None))
-    } yield tracks
+  private def parseVisibleTrackIds(doc : JsoupDocument, html : String) : Either[UnexpectedServiceResponse, List[Int]] = {
+    doc >?> elements("li.track") match {
+      case None => Left(UnexpectedServiceResponse("Could not find tracks on page", html, None))
+      case Some(els) => Right(els.map(el => el.attr("data-ec-id").toInt).toList)
+    }
   }
 
-  def parsePager(doc : JsoupDocument) : Either[UnexpectedServiceResponse, Option[BeatportPager]] = {
+  private def parseTracksJson(doc : JsoupDocument, html : String) : Either[UnexpectedServiceResponse, List[BeatportPageTrack]] = {
+    doc >?> element("script#data-objects") match {
+      case None => Left(UnexpectedServiceResponse("Could not find script#data-objects", html, None))
+      case Some(el) => {
+        val script = el.innerHtml
+        val playablesPrefix = "window.Playables = "
+        val playablesIndex = script.indexOf(playablesPrefix)
+        if (playablesIndex == -1)
+          Left(UnexpectedServiceResponse(s"Could not find $playablesPrefix", html, None))
+        else {
+          val jsonStr = script.substring(playablesIndex + playablesPrefix.length, script.indexOf("};") + 1)
+          for {
+            json <- circe.parser.parse(jsonStr)
+              .left.map(pf => UnexpectedServiceResponse(pf.message, jsonStr, None))
+            tracks <- json.hcursor.downField("tracks").as[List[BeatportPageTrack]]
+              .left.map(dr => UnexpectedServiceResponse(dr.message ++ CursorOp.opsToPath(dr.history), jsonStr, None))
+          } yield tracks
+        }
+      }
+    }
+  }
+
+  private def parsePager(doc : JsoupDocument) : Either[UnexpectedServiceResponse, Option[BeatportPager]] = {
     doc >?> element("div.pag-numbers") match {
       case None => Right(None)
       case Some(el) =>
         val currentPg = (el >> element("strong.pag-number-current")).text.toInt
-        val lastPg = (el >> elements("a.pag-number")).last.text.toInt
+        val lastPg = (el >> elements(".pag-number")).last.text.toInt
         Right(Some(BeatportPager(currentPg, lastPg)))
     }
   }
@@ -92,9 +99,10 @@ object BeatportPage {
         mix         <- c.downField("mix").as[String]
         release     <- c.downField("release").downField("name").as[String]
         label       <- c.downField("label").downField("name").as[String]
-        duration    <- c.downField("duration").downField("milliseconds").as[Int].map(ms => Duration.ofMillis(ms))
+        duration    <- c.downField("duration").downField("milliseconds").as[Option[Int]]
+                        .map(mso => mso.map(ms => Duration.ofMillis(ms)))
         previewUrl  <- c.downField("preview").downField("mp3").downField("url").as[Uri]
-        key         <- c.downField("key").as[String]
+        key         <- c.downField("key").as[Option[String]]
       } yield BeatportPageTrack(id, artists, title, name, mix, release, label, duration, previewUrl, key)
   }
 }
--- src/test/scala/pd2/providers/test/BeatportPageSuite.scala
+++ src/test/scala/pd2/providers/test/BeatportPageSuite.scala
@@ -1,16 +1,118 @@
 package pd2.providers.test
 
 
+import pd2.providers.beatport.{BeatportPage, BeatportPageArtist, BeatportPageTrack, BeatportPager}
+import sttp.model.Uri
+import zio.ZIO
 import zio.test._
-import zio.test.Assertion.anything
+import zio.test.Assertion.{anything, equalTo, isNone, isSome}
 import zio.test.DefaultRunnableSpec
+
+import java.time.Duration
 
 object BeatportPageSuite extends DefaultRunnableSpec with ManagedTestResources {
 
   override def spec =
-    suite("TraxsourceDataProviderSuite")(
-      test("Traxsource_JustAdded_FirstPage.html") {
-        assert(())(anything)
+    suite("BeatportPageSuite")(
+
+      testM("Beatport_Top100.html") {
+
+        val expectedFirstTrack = BeatportPageTrack(
+          id = 15206183,
+          artists = List(BeatportPageArtist(42511, "Piero Pirupa", "piero-pirupa")),
+          title = "",
+          name = "Everybody's Free (To Feel Good)",
+          mix = "Deeper Purpose Extended Remix",
+          release = "Everybody's Free (To Feel Good) [Deeper Purpose Extended Remix]",
+          label = "SPINNIN' DEEP",
+          duration = Some(Duration.ofMillis(366724)),
+          previewUrl = Uri.parse("https://geo-samples.beatport.com/track/53db3f90-ba5a-4b0b-aa0d-be58c13a9fb9.LOFI.mp3").right.get,
+          key = Some("A min")
+        )
+
+        val expectedSeventhTrack = BeatportPageTrack(
+          id = 15166533,
+          artists = List(BeatportPageArtist(419524, "Kideko", "kideko"), BeatportPageArtist(648493, "Saffron Stone", "saffron-stone")),
+          title = "",
+          name = "The Music",
+          mix = "Extended Mix",
+          release = "The Music",
+          label = "Armada Subjekt",
+          duration = Some(Duration.ofMillis(330254)),
+          previewUrl = Uri.parse("https://geo-samples.beatport.com/track/a1cee24e-c80e-4992-bc80-f6ef83504785.LOFI.mp3").right.get,
+          key = Some("D min")
+        )
+
+        loadTextFileManaged("/Beatport_Top100.html.zip")
+          .use { doc => ZIO.fromEither(BeatportPage.parse(doc)) }
+          .map { res =>
+            assert(res.pager)(isNone) &&
+            assert(res.tracks.length)(equalTo(100)) &&
+            assert(res.tracks.head)(equalTo(expectedFirstTrack)) &&
+            assert(res.tracks.drop(6).head)(equalTo(expectedSeventhTrack))
+          }
+      },
+
+      testM("Beatport_Tracks_FirstPage.html") {
+
+        val expectedFirstTrack = BeatportPageTrack(
+          id = 14806028,
+          artists = List(BeatportPageArtist(696433, "Rocho Sainkt", "rocho-sainkt")),
+          title = "",
+          name = "Arzukela",
+          mix = "House Mix",
+          release = "House Ocean",
+          label = "Berry Parfait",
+          duration = Some(Duration.ofMillis(270094)),
+          previewUrl = Uri.parse("https://geo-samples.beatport.com/track/1337b5e7-db28-4b7e-b29c-102d367731de.LOFI.mp3").right.get,
+          key = Some("A min")
+        )
+
+        val expectedLastTrack = BeatportPageTrack(
+          id = 14806043,
+          artists = List(BeatportPageArtist(679217, "Dyba", "dyba"), BeatportPageArtist(696442, "Sound Expander", "sound-expander")),
+          title = "",
+          name = "Technically Speaking feat. Sound Expander",
+          mix = "Original Mix",
+          release = "House Ocean",
+          label = "Berry Parfait",
+          duration = Some(Duration.ofMillis(191219)),
+          previewUrl = Uri.parse("https://geo-samples.beatport.com/track/a3955c7d-c2c4-46f6-b20c-99c1de2f5541.LOFI.mp3").right.get,
+          key = Some("D min")
+        )
+
+        loadTextFileManaged("/Beatport_Tracks_FirstPage.html.zip")
+          .use { doc => ZIO.fromEither(BeatportPage.parse(doc)) }
+          .map { res =>
+            assert(res.pager)(isSome(equalTo(BeatportPager(1, 66)))) &&
+              assert(res.tracks.length)(equalTo(25)) &&
+              assert(res.tracks.head)(equalTo(expectedFirstTrack)) &&
+              assert(res.tracks.drop(24).head)(equalTo(expectedLastTrack))
+          }
+      },
+
+      testM("Beatport_Tracks_MiddlePage.html") {
+
+        loadTextFileManaged("/Beatport_Tracks_MiddlePage.html.zip")
+          .use { doc => ZIO.fromEither(BeatportPage.parse(doc)) }
+          .map { res =>
+            assert(res.pager)(isSome(equalTo(BeatportPager(30, 66)))) &&
+              assert(res.tracks.length)(equalTo(25)) &&
+              assert(res.tracks.head.id)(equalTo(15129444)) &&
+              assert(res.tracks(24).id)(equalTo(15133827)) &&
+              assert(res.tracks(18).duration)(isNone) &&
+              assert(res.tracks(18).key)(isNone)
+          }
+      },
+
+      testM("Beatport_Tracks_MiddlePage.html") {
+
+        loadTextFileManaged("/Beatport_Tracks_LastPage.html.zip")
+          .use { doc => ZIO.fromEither(BeatportPage.parse(doc)) }
+          .map { res =>
+            assert(res.pager)(isSome(equalTo(BeatportPager(66, 66)))) &&
+              assert(res.tracks.length)(equalTo(12))
+          }
       }
     )
 }
