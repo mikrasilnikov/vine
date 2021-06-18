026e4420e6c8555612d7d353ab485833bd3ce31a
mkrasilnikov <mi.krasilnikov@gmail.com>
2021-06-18 03:01:07 +0300

    *

---
+++ bundle/run.bat
@@ -0,0 +1,2 @@
+java -jar vine.jar --genres=my,house --date-2021-06-18
+pause
---
+++ bundle/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -jar vine.jar --genres=my,house --date-2021-06-18
+read -n1 -r -p "Press any key to continue..." key
